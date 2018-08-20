7e00c18c7dd7703f3c2190bbc0e8b5f99f45457a
aoxiaodie <1070508412>
2018-08-20 21:03:24 +0800

    second commit

---
+++ Assets_final/Script/EvnGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvnGenerator : MonoBehaviour {
+
+	public GameObject[] pathpack;
+
+	private int pathCount = 2;
+
+	public Path path_01;
+	public Path path_02;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	Path GeneratePath()
+	{
+		pathCount++;
+		float z = 400 * pathCount;
+		int type = Random.Range(0, 2);
+		GameObject newPath = GameObject.Instantiate(pathpack[type], new Vector3(0, 0, z), Quaternion.identity);
+		return newPath.GetComponent<Path>();
+	}
+
+	public void UpadatePath()
+	{
+		path_01 = path_02;
+		path_02 = GeneratePath();
+	}
+}
